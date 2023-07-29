b154d8e033137d0eebb57546c38a25b40d8608ba
yangyuewei <yangyuewei40@outlook.com>
2023-07-30 01:02:59 +0800

    005

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,12 +4,24 @@
         <Carousel />
         <!-- 首页搜索表单 -->
         <Search />
-    </div>
+        <!-- 首页医院内容区域 -->
+        <el-row :gutter="20">
+            <el-col :span="20">
+                <!-- 等级子组件 -->
+                <Level />
+                <!-- 地区子组件 -->
+                <Region />
+            </el-col>
+            <el-col :span="4">456</el-col>
+        </el-row>
+    </div> 
 </template>
 
 <script setup lang='ts'>
 import Carousel from './carousel/index.vue'
 import Search from './search/index.vue'
+import Level from './level/index.vue'
+import Region from './region/index.vue'
 </script>
 
 <style scoped lang="scss"></style>
